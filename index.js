add66a855957976be0cefa7e0b4a5a0c5636702d
Vaishnavi-N-08 <vaishnavinarkhede8@gmail.com>
2022-07-26 16:52:16 +0530

    some changes

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 
 const express = require('express');
 const app = express();
-const { readFile } = require('fs').promises;
+// const { readFile } = require('fs').promises;
  
-app.get('/',async (request,response) =>{
-
-response.send(await readFile('./home.html','utf8'));
-});
+// app.get('/',async (request,response) =>{
+// response.send(await readFile('./home.html','utf8'));
+// });
+app.use(express.static("public"))
 app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
 
 module.exports = app
